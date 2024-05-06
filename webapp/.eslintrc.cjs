65ecf4b830bb0751de961687a5f22328fdc8bbde
Subham Kumar <skuma251@asu.edu>
2024-05-05 21:56:26 -0700

    🧱 added linting and corrected vite config

--- webapp/.eslintrc.cjs
+++ webapp/.eslintrc.cjs
@@ -1,18 +1,38 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+      browser: true,
+      es2021: true,
   },
-}
+  extends: ['standard', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'prettier'],
+  overrides: [
+      {
+          env: {
+              node: true,
+          },
+          files: ['.eslintrc.{js,cjs}'],
+          parserOptions: {
+              sourceType: 'script',
+          },
+      },
+  ],
+  parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+      // Customize rules as per your project's needs
+      'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
+      'react/react-in-jsx-scope': 'off', // React is included in the JSX runtime in React 17+
+      'react/jsx-uses-react': 'off', // React is not defined when using JSX runtime
+      'react/jsx-uses-vars': 'warn', // Warn if variables used in JSX are not defined
+      'no-unused-vars': 'warn', // Warn about unused variables
+      'indent': ['error', 4], // Enforce consistent indentation with 4 spaces
+      'semi': ['error', 'always'], // Enforce the use of semicolons at the end of each statement
+  },
+  settings: {
+      react: {
+          version: 'detect', // Automatically includes the React version
+      },
+  },
+};
--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { URL, fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,16 +11,16 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+        find: '@',
+        replacement: new URL('./src', import.meta.url).pathname,
       },
       {
-        find: "@assets",
-        replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
+        find: '@assets',
+        replacement: new URL('./src/assets', import.meta.url).pathname,
       },
       {
-        find: "@components",
-        replacement: fileURLToPath(new URL("./src/components", import.meta.url)),
+        find: '@components',
+        replacement: new URL('./src/components', import.meta.url).pathname,
       },
     ],
   },
