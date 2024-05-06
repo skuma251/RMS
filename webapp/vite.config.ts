b0ed5aefe8d1ca1aae60541711197e83ebebc3eb
Subham Kumar <skuma251@asu.edu>
2024-05-05 21:41:20 -0700

    🧑‍💻 updated vite configs

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { URL, fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/rms/",
+  build: {
+    outDir: "build",
+  },
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+      {
+        find: "@assets",
+        replacement: fileURLToPath(new URL("./src/assets", import.meta.url)),
+      },
+      {
+        find: "@components",
+        replacement: fileURLToPath(new URL("./src/components", import.meta.url)),
+      },
+    ],
+  },
 })
